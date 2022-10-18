76df9869708d034cd1a4bad5ea80507a517a086c
Suman2024 <sumanmani2024@gmail.com>
2022-10-18 12:59:19 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from app.models import *
+# Create your views here.
+def display_topic(request):
+    T=Topic.objects.all()
+    d={'topics':T}
+    return render(request,'display_topic.html',d)
+
+
+def display_webpage(request):
+    W=Webpage.objects.all()
+    d={'Webpages':W}
+    return render(request,'display_webpage.html',d)
